283a8d583b501073bca7ea0d4ccd2107950ab417
Juha Itäleino <jsilvanus@gmail.com>
2026-08-22 22:41:37 +0300

    feat: expose treeless write APIs

--- hotgit/__init__.py
+++ hotgit/__init__.py
@@ -1,5 +1,15 @@
 """Fast, treeless Git repository access."""
 
+from .commits import CommitBuilder, CommitIdentity
+from .objects import ObjectStore
 from .repository import Repository
+from .trees import TreeBuilder, TreeEntry
 
-__all__ = ["Repository"]
+__all__ = [
+    "CommitBuilder",
+    "CommitIdentity",
+    "ObjectStore",
+    "Repository",
+    "TreeBuilder",
+    "TreeEntry",
+]
