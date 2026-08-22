3e34bd4910993507015f3b2d830354194187093f
Juha Itäleino <jsilvanus@gmail.com>
2026-08-22 23:06:12 +0300

    feat: expose integration errors in public API

--- hotgit/__init__.py
+++ hotgit/__init__.py
@@ -3,8 +3,8 @@
 from .commits import CommitBuilder, CommitIdentity
 from .edit import Change, EditResult, Editor
 from .objects import ObjectStore
-from .refs import RefStore
-from .repository import Repository
+from .refs import RefConflictError, RefStore
+from .repository import ObjectNotFoundError, Repository, RepositoryError
 from .trees import TreeBuilder, TreeEntry
 from .worker import RepositoryWorker, WorkerPool
 
@@ -14,9 +14,12 @@ __all__ = [
     "CommitIdentity",
     "EditResult",
     "Editor",
+    "ObjectNotFoundError",
     "ObjectStore",
+    "RefConflictError",
     "RefStore",
     "Repository",
+    "RepositoryError",
     "RepositoryWorker",
     "TreeBuilder",
     "TreeEntry",
