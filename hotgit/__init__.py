5482e1fe5c3bca1368314cd0b70e8e139a95b1e8
Juha Itäleino <jsilvanus@gmail.com>
2026-08-26 15:36:08 +0300

    feat: export commit history API

--- hotgit/__init__.py
+++ hotgit/__init__.py
@@ -2,6 +2,7 @@
 
 from .commits import CommitBuilder, CommitIdentity
 from .edit import Change, EditResult, Editor
+from .history import Commit, History, Identity, Ref
 from .objects import ObjectStore
 from .refs import RefConflictError, RefStore
 from .repository import ObjectNotFoundError, Repository, RepositoryError
@@ -10,12 +11,16 @@ from .worker import RepositoryWorker, WorkerPool
 
 __all__ = [
     "Change",
+    "Commit",
     "CommitBuilder",
     "CommitIdentity",
     "EditResult",
     "Editor",
+    "History",
+    "Identity",
     "ObjectNotFoundError",
     "ObjectStore",
+    "Ref",
     "RefConflictError",
     "RefStore",
     "Repository",
